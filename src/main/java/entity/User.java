2e4118404be70bb65cfb2c99e551981f9465ac86
jimsonlung <514989514@qq.com>
2018-10-29 20:13:09 +0800

    package

---
+++ src/main/java/entity/User.java
@@ -0,0 +1,84 @@
+package main.java.entity;
+
+/**
+ * 用户类
+ *
+ */
+public class User {
+    private String id;
+    private String name;
+    private char sex;
+    private int tel;
+    private String password;
+    private String email;
+    private String question;
+    private String answer;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public char getSex() {
+        return sex;
+    }
+
+    public void setSex(char sex) {
+        this.sex = sex;
+    }
+
+    public int getTel() {
+        return tel;
+    }
+
+    public void setTel(int tel) {
+        this.tel = tel;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getQuestion() {
+        return question;
+    }
+
+    public void setQuestion(String question) {
+        this.question = question;
+    }
+
+    public String getAnswer() {
+        return answer;
+    }
+
+    public void setAnswer(String answer) {
+        this.answer = answer;
+    }
+
+    public void test(){
+        System.out.println("user test.");
+    }
+}
---
+++ src/main/java/test/ClassTest.java
@@ -0,0 +1,20 @@
+package main.java.test;
+
+import main.java.entity.User;
+import org.junit.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+/**
+ * 测试类
+ *
+ */
+public class ClassTest {
+
+    @Test
+    public void test1(){
+        ApplicationContext ac = new ClassPathXmlApplicationContext("main/resources/applicationContext.xml");
+        User user = (User) ac.getBean("user");
+        user.test();
+    }
+}
